07fc1d532b1452007f381ebada69fd99e491109f
Luis Cruz <lfcruz.lopez@gmail.com>
2022-09-02 14:18:59 -0700

    Section 10 Changes

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using System.Collections.Generic;
 using API.Data;
 using API.Entities;
@@ -41,6 +42,21 @@ namespace API.Controllers
             return  user;
         }
 
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            var user = await _userRepository.GetUserByUsernameAsync(username);
+
+            _mapper.Map(memberUpdateDto, user);
+
+            _userRepository.Update(user);
+
+            if(await _userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update user");
+        }
 
 
     }
--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -9,14 +9,18 @@ import { MessagesComponent } from './messages/messages.component';
 import { AuthGuard } from './_guards/auth.guard';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { PreventUnsavedChangesGuard } from './_guards/prevent-unsaved-changes.guard';
 
 const routes: Routes = [
 {path:'', component:HomeComponent},{
   path:'',
-  runGuardsAndResolvers:'always',
+  runGuardsAndResolvers:'always', 
+  canActivate: [AuthGuard],
   children:[
-    {path:'members', component:MemberListComponent, canActivate: [AuthGuard]},
-    {path:'members/:username', component:MemberDetailComponent, canActivate: [AuthGuard]},
+    {path:'members', component:MemberListComponent},
+    {path:'members/:username', component:MemberDetailComponent},
+    {path:'member/edit', component:MemberEditComponent, canDeactivate:[PreventUnsavedChangesGuard]},
     {path:'lists', component:ListsComponent},
     {path:'message', component:MessagesComponent},
 
